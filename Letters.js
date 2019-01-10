5fba3699c741cbb5d13fe83b51216e555698dd08
Jose Hernandez Torres <jose@hernandezt.com>
2019-01-09 18:36:34 -0700

    Installed Dependencies

---
+++ Letters.js
@@ -0,0 +1,19 @@
+class Letter {
+    constructor(letter){
+        // string to store underline for letters
+        this.letter = letter;
+        // boolean to store if letter has been guessed
+        this.guessed = false;
+        
+    }
+        // function to return underlying character if letter has been guessed 
+            // or shows underline if letter has not been guessed
+        incorrectGuess(){
+
+        }
+        // function that takes a character as an argument and checks it against
+            // the underlined character, updating boolean to true if guessed correctly
+        correctGuess(){
+
+        }
+}
